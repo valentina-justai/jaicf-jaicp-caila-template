4e3112ba35d182a354f39fc4a219ef6006082c45
valentina-justai <80454040+valentina-justai@users.noreply.github.com>
2022-03-24 10:14:11 +0300

    Update MainScenario.kt

--- src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
+++ src/main/kotlin/com/justai/jaicf/template/scenario/MainScenario.kt
@@ -13,8 +13,8 @@ val mainScenario = Scenario {
             reactions.run {
                 image("https://media.giphy.com/media/ICOgUNjpvO0PC/source.gif")
                 sayRandom(
-                    "Hello! How can I help?",
-                    "Hi there! How can I help you?"
+                    "Привет! Чем я могу вам помочь?",
+                    "Здорово! Как дела?"
                 )
                 buttons(
                     "Help me!",
